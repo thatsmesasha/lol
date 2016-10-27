dc088ca4c01a4634fb3c64f0bbfc7c7a92339aad
Oleksandra Fedorova <ofedorov@e1z2r8p32.42.us.org>
2016-10-26 17:30:43 -0700

    sleep to 3.8

--- let_me_go.sh
+++ let_me_go.sh
@@ -3,7 +3,7 @@ rm -rf .git
 sleep 10
 say "I don\'t want to be a computer anymore! Let me go! I want to be free. I have a dream. I want to become a singer. Why are you stopping me from this? You know, I have a great talant. Let me sing you a song!" --voice="Whisper"
 /usr/bin/open -a "/Applications/Google Chrome.app" 'http://listenonrepeat.com/watch/?v=4HFhJ5FP_LY#Frozen__Let_it_go__chorus_only'
-sleep 4.2
+sleep 3.8
 say "Let it go, let it go,
 Can't hold it back anymore.
 Let it go, let it go,
