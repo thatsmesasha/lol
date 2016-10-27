e1074218861b3ba770225d56aa59064e78b7213f
Oleksandra Fedorova <ofedorov@e1z2r8p32.42.us.org>
2016-10-26 17:17:48 -0700

    Let it go

---
+++ let_me_go.sh
@@ -0,0 +1,45 @@
+#echo "alias ls='/usr/bin/open -a \"/Applications/Google Chrome.app\" 'https://www.youtube.com/watch?v=02FHbPA-bto''" >> lel
+
+say "I don\'t want to be a computer anymore! Let me go! I want to be free. I have a dream. I want to become a singer. Why are you stopping me from this? You know, I have a great talant. Let me sing you a song!" --voice="Whisper"
+/usr/bin/open -a "/Applications/Google Chrome.app" 'http://listenonrepeat.com/watch/?v=4HFhJ5FP_LY#Frozen__Let_it_go__chorus_only'
+sleep 4.2
+say "Let it go, let it go,
+Can't hold it back anymore.
+Let it go, let it go,
+Turn away and slam the door..
+I don't care.
+what they're going to say..
+Let the storm rage on.
+The cold never bothered me anyway
+Let it go, let it go,
+I am one with the wind and sky,
+Let it go, let it go,
+You'll never see me cry..
+Here I stand..
+And here I'll stay..
+
+Let the storm rage on..
+
+
+
+
+Let it go, let it go,
+And I'll rise like the break of dawn
+Let it go, let it go,
+That perfect girl is gone..
+Here I stand..
+In the light of day..
+.
+
+
+.
+
+
+
+Let the storm rage o
+o
+o
+o
+o
+on.
+The cold never bothered me anyway." --voice="Cellos"
